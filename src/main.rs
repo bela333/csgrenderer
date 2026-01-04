0b718fe0c92e7279e19628d2d0ceecc4fda9c686
bela333 <b3kstudio@gmail.com>
2026-01-04 23:46:30 +0100

    animation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::f32;
 
-use glam::{Affine3A, Vec3, vec3};
+use glam::{Affine3A, Mat3, Vec3, vec3};
 use image::RgbImage;
 
 use crate::{
@@ -17,8 +17,8 @@ pub mod range_intersect;
 pub mod range_union;
 pub mod range_vec_union;
 
-const WIDTH: u32 = 1920;
-const HEIGHT: u32 = 1080;
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
 
 const ASPECT_RATIO: f32 = WIDTH as f32 / HEIGHT as f32;
 
@@ -49,58 +49,60 @@ fn main() {
         CSGDifference::new(sphere, holes)
     };
 
-    let camera = Affine3A::look_at_lh(
-        vec3(3.0, 2.0, 0.0),
-        vec3(0.0, 0.0, 0.0),
-        vec3(0.0, 1.0, 0.0),
-    )
-    .inverse();
     let light = vec3(2.0, 2.0, -2.0) * 100.0;
+    for frame in 0..180 {
+        let camera = Affine3A::look_at_lh(
+            Mat3::from_rotation_y(((frame * 2) as f32).to_radians()) * vec3(3.0, 2.0, 0.0),
+            vec3(0.0, 0.0, 0.0),
+            vec3(0.0, 1.0, 0.0),
+        )
+        .inverse();
 
-    let camera_origin: Vec3 = camera.translation.into();
-    let mut buf = vec![[None; WIDTH as usize]; HEIGHT as usize];
+        let camera_origin: Vec3 = camera.translation.into();
+        let mut buf = vec![[None; WIDTH as usize]; HEIGHT as usize];
 
-    for (y, row) in buf.iter_mut().enumerate() {
-        for (x, p) in row.iter_mut().enumerate() {
-            let x = x as f32 / WIDTH as f32;
-            let y = y as f32 / HEIGHT as f32;
-            let x = (x - 0.5) * 2.0 * ASPECT_RATIO;
-            let y = ((1.0 - y) - 0.5) * 2.0;
-            let direction = camera.transform_vector3(Vec3::new(x, y, 2.0)).normalize();
-            let i = o.trace(camera_origin, direction);
-            let mut i = RangeIntersect::new(i, vec![0.0, f32::INFINITY].into_iter());
-            *p = i.next().map(|d| direction * d + camera_origin)
-        }
-    }
-    let mut img = RgbImage::new(WIDTH, HEIGHT);
-    for (x, y, p) in img.enumerate_pixels_mut() {
-        let v = buf[y as usize][x as usize];
-        let v = match v {
-            Some(v) => v,
-            None => {
-                *p = image::Rgb([0, 0, 0]);
-                continue;
+        for (y, row) in buf.iter_mut().enumerate() {
+            for (x, p) in row.iter_mut().enumerate() {
+                let x = x as f32 / WIDTH as f32;
+                let y = y as f32 / HEIGHT as f32;
+                let x = (x - 0.5) * 2.0 * ASPECT_RATIO;
+                let y = ((1.0 - y) - 0.5) * 2.0;
+                let direction = camera.transform_vector3(Vec3::new(x, y, 2.0)).normalize();
+                let i = o.trace(camera_origin, direction);
+                let mut i = RangeIntersect::new(i, vec![0.0, f32::INFINITY].into_iter());
+                *p = i.next().map(|d| direction * d + camera_origin)
             }
-        };
-        let normal = {
-            let vx = buf[y as usize]
-                .get(x as usize + 1)
-                .unwrap_or(&None)
-                .unwrap_or(v);
-            let vy = buf
-                .get(y as usize + 1)
-                .map(|v| v[x as usize])
-                .unwrap_or(None)
-                .unwrap_or(v);
-            (v - vx).cross(v - vy).normalize()
-        };
-        let diffuse = (light - v).normalize().dot(normal).clamp(0.0, 1.0);
-        let color = Vec3::ONE * (diffuse + 0.01).powf(1.0 / 2.2);
-        *p = image::Rgb([
-            (color.x.clamp(0.0, 1.0) * 255.0) as u8,
-            (color.y.clamp(0.0, 1.0) * 255.0) as u8,
-            (color.z.clamp(0.0, 1.0) * 255.0) as u8,
-        ])
+        }
+        let mut img = RgbImage::new(WIDTH, HEIGHT);
+        for (x, y, p) in img.enumerate_pixels_mut() {
+            let v = buf[y as usize][x as usize];
+            let v = match v {
+                Some(v) => v,
+                None => {
+                    *p = image::Rgb([0, 0, 0]);
+                    continue;
+                }
+            };
+            let normal = {
+                let vx = buf[y as usize]
+                    .get(x as usize + 1)
+                    .unwrap_or(&None)
+                    .unwrap_or(v);
+                let vy = buf
+                    .get(y as usize + 1)
+                    .map(|v| v[x as usize])
+                    .unwrap_or(None)
+                    .unwrap_or(v);
+                (v - vx).cross(v - vy).normalize()
+            };
+            let diffuse = (light - v).normalize().dot(normal).clamp(0.0, 1.0);
+            let color = Vec3::ONE * (diffuse + 0.01).powf(1.0 / 2.2);
+            *p = image::Rgb([
+                (color.x.clamp(0.0, 1.0) * 255.0) as u8,
+                (color.y.clamp(0.0, 1.0) * 255.0) as u8,
+                (color.z.clamp(0.0, 1.0) * 255.0) as u8,
+            ])
+        }
+        img.save(format!("frames/frame{frame:04}.png")).unwrap();
     }
-    img.save("output.png").unwrap();
 }
